74535fa2ecb80e35a223d7a119276f8fd07217fb
Ravshan007 <ravshanaliyev9002@gmail.com>
2023-12-20 13:10:06 +0500

    some changes in tailwind config

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import {faker} from '@faker-js/faker'
 function App() {
+  const words = faker.random.words(10)
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
+      <h1 className="text-4xl font-bold  text-slate-500">
+        {words}
       </h1>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary:  colors.yellow
+      }
+    },
   },
   plugins: [],
 };
